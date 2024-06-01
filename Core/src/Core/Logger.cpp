be67cd906dae23ece2f8fb51e796d040cd324153
Iannis Poquet <ianpo@live.fr>
2024-06-01 13:53:48 +0200

    Init Logger.cpp

--- Core/src/Core/Logger.cpp
+++ Core/src/Core/Logger.cpp
@@ -3,6 +3,36 @@
 //
 
 #include "Core/Logger.hpp"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include <memory>
 
 namespace Core {
+   std::shared_ptr<spdlog::logger> Logger::s_CoreLogger;
+
+   void Logger::Init()
+   {
+      // Setting Pattern
+      spdlog::set_pattern("%^[%T] [%l] %n (%s:%#->%!): %v%$");
+
+      // Creating core logger
+      std::array<spdlog::sink_ptr, 2> core_sinks;
+      core_sinks[0] = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+      core_sinks[1] = std::make_shared<spdlog::sinks::basic_file_sink_mt>("application.log");
+
+      s_CoreLogger = std::make_shared<spdlog::logger>("APP", core_sinks.begin(), core_sinks.end());
+      s_CoreLogger->set_level(spdlog::level::trace);
+
+      s_Init = true;
+   }
+
+   void Logger::Destroy() {
+      s_CoreLogger = nullptr;
+      s_Init = false;
+   }
+
+   std::shared_ptr<spdlog::logger>& Logger::GetLogger() {
+      if(!s_Init) Init();
+      return s_CoreLogger;
+   }
 } // Core
